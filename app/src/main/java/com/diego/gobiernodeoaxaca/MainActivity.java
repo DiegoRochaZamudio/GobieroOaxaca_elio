b5676e3c62322d34554b4d20c7b83d9415f50535
DiegoRochaZamudio <rocha.diego.z96@gmail.com>
2019-11-19 11:44:22 -0600

    1.0.0

--- app/src/main/java/com/diego/gobiernodeoaxaca/MainActivity.java
+++ app/src/main/java/com/diego/gobiernodeoaxaca/MainActivity.java
@@ -3,6 +3,7 @@ package com.diego.gobiernodeoaxaca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -12,7 +13,7 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText numHijos, numHijosEscolares;
+    private EditText edTxt1, edTxt2;
     private Button generar;
     private RadioButton viuda, otro;
     private TextView totalH, totalHE, viudez, subsidioTotal;
@@ -23,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        numHijos = (EditText)findViewById(R.id.edTxt1);
-        numHijosEscolares = (EditText)findViewById(R.id.edTxt2);
+        edTxt1 = (EditText) findViewById(R.id.edTxt1);
+        edTxt2 = (EditText)findViewById(R.id.edTxt2);
         generar = (Button)findViewById(R.id.btn1);
         viuda = (RadioButton)findViewById(R.id.rButton1);
         otro = (RadioButton)findViewById(R.id.rButton2);
@@ -32,5 +33,53 @@ public class MainActivity extends AppCompatActivity {
         totalHE = (TextView) findViewById(R.id.hijosEscolares);
         viudez = (TextView) findViewById(R.id.viudez);
         subsidioTotal = (TextView) findViewById(R.id.subsidioTotal);
+
+
+        generar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                generar_Subsidio();
+            }
+        });
+
+
+
+    }
+
+    public void generar_Subsidio(){
+
+        int nHijos, nHijosEscolares;
+        double sub1, sub2, sub3, subTotal;
+
+        String numHijos = edTxt1.getText().toString();
+        nHijos = Integer.parseInt(numHijos);
+
+        String numHijosEscolares = edTxt2.getText().toString();
+        nHijosEscolares = Integer.parseInt(numHijosEscolares);
+
+        if (nHijos <= 2){
+            sub1= 1700;
+
+        }else if (nHijos <= 5){
+            sub1 = 1900;
+        }else
+            sub1 = 1200;
+
+
+        sub2 = nHijosEscolares * 1200;
+
+        sub3 = 0;
+
+        if(viuda.isChecked() == true){
+            sub3 = 800;
+        }
+
+        subTotal = sub1 + sub2 + sub3;
+
+        totalH.setText("Monto por total de hijos: " + sub1);
+        totalHE.setText("Monto por hijos en edad escolar: " + sub2);
+        viudez.setText("Monto por viudez: " + sub3);
+        subsidioTotal.setText("Subsidio Total: " +subTotal);
     }
 }
